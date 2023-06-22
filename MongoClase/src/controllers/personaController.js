035d2db6053d19edfea87ff5e20dfa2ce1943a77
Aparevalo <anarevalovi@uide.com.ec>
2023-06-21 20:00:07 -0500

    mongoClase

--- MongoClase/src/controllers/personaController.js
+++ MongoClase/src/controllers/personaController.js
@@ -1,24 +1,73 @@
-var Persona = require('../models/persona');
-var express = require('express');
-var router = express.Router();
+var persona = require('../models/persona');
 
-router.post('/crearPersona', async (request, response) => {
-  try {
-    var body = request.body;
-    await Persona.insertMany({
-      nombres: body.nombres,
-      apellidos: body.apellidos,
-      edad: body.edad,
-      genero: body.genero,
-      estadoCivil: body.estadoCivil,
-      descripcion: body.descripcion,
-      telefono: body.telefono
-    });
-    response.status(200).json('Persona creada correctamente');
-  } catch (err) {
-    console.log(err);
-    response.status(500).json('Ocurrió un error al crear la persona');
-  }
-});
+const crearPersona = async (req, res) => {
+  
+    const {nombres,apellidos,edad,genero,estadoCivil,descripcion,telefono} = req.body;
+  
+    try {
+      const nuevaPersona = await persona.create({ nombres,apellidos,edad,genero,estadoCivil,descripcion,telefono});
+      res.json(nuevaPersona);
+    } catch (error) {
+      res.status(500).json({ error: 'Error al crear la persona' });
+    }
+  };
 
-module.exports = router;
+  
+  const obtenerPersona = async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      const personaId = await persona.findByIdAndRemove(id);
+  
+      if (!personaId) {
+        return res.status(404).json({ error: 'Persona no encontrada' });
+      }
+      
+      
+    } catch (error) {
+      res.status(500).json({ error: 'Error al encontrar la persona' });
+    }
+  };
+
+  const obtenerPersonas = async (req, res) => {
+    try {
+      const personas = await persona.find();
+      res.json(personas);
+    } catch (error) {
+      res.status(500).json({ error: 'Error al obtener las personas' });
+    }
+  };
+
+
+
+  const actualizarPersona = async (req, res) => {
+    const { id } = req.params;
+    const { nombres,apellidos,edad,genero,estadoCivil,descripcion,telefono } = req.body;
+  
+    try {
+      const personaActualizado = await persona.findByIdAndUpdate(
+        id,
+        { nombres,apellidos,edad,genero,estadoCivil,descripcion,telefono },
+        { new: true }
+      );
+      res.json(personaActualizado);
+    } catch (error) {
+      res.status(500).json({ error: 'Error al actualizar la persona' });
+    }
+  };
+  
+  
+  const eliminarPersona = async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      await persona.findByIdAndRemove(id);
+      res.json({ message: 'Persona eliminada exitosamente' });
+    } catch (error) {
+      res.status(500).json({ error: 'Error al eliminar la persona' });
+    }
+  };
+
+
+  module.exports ={crearPersona, obtenerPersona , obtenerPersonas, eliminarPersona, actualizarPersona};
+
